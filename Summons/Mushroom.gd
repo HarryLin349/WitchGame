145aa6d1db33861d0edb8b2940cd4ad924210808
isabellesprout <150958560+isabellesprout@users.noreply.github.com>
2024-10-06 17:03:44 -0400

    mushroom!

--- Summons/Mushroom.gd
+++ Summons/Mushroom.gd
@@ -1,4 +1,4 @@
-extends "res://Summons/summon.gd"
+extends Summon
 
 class_name Mushroom 
 # Bee-specific summon properties
@@ -8,10 +8,11 @@ func _ready():
 	speed = 0
 	summonCost = 5
 	animated_sprite = $AnimatedSprite2D
-	duration = 5.0  # Lasts for 5 seconds
+	duration = 10.0  # Lasts for 10 seconds
 
 # Override movement behavior (optional)
 func _process(delta):
 	# The mushroom doesn't move
 	# Call the parent _process to handle the time-based removal
+	go_to_nearest_enemy()
 	super(delta)
--- summoning.gd
+++ summoning.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
 @onready var beeScene = preload("res://Summons/Bee.tscn")
+@onready var mushroomScene = preload("res://Summons/Mushroom.tscn")
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		# Check if the left mouse button was clicked and if you have enough mana
 		if event.pressed and GlobalStats.Mana >= Bee.summonCost:
 			var world_position = get_global_mouse_position()
-			spawn_bee(world_position)
+			spawn_bee(world_position) if randf() < 0.5 else spawn_mushroom(world_position)
 			#spawn_mushroom(world_position)
 			GlobalStats.decrease_mana(Bee.summonCost)  # Deduct mana when a bee is summoned
 		else:
@@ -18,10 +19,9 @@ func spawn_bee(position: Vector2):
 	var bee_instance = beeScene.instantiate()
 	bee_instance.position = position
 	add_child(bee_instance)
-"""
+	
 func spawn_mushroom(position: Vector2):
 	# Instantiate the bee and set its position to the mouse click location
 	var mushroom_instance = mushroomScene.instantiate()
 	mushroom_instance.position = position
 	add_child(mushroom_instance)
-"""
